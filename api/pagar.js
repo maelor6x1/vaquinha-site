e1afbea065181ead497d6c4066f5bc142612199a
maelor6x1 <r5y2vnn98p@privaterelay.appleid.com>
2026-05-27 04:49:37 -0300

    Update pagar.js

--- api/pagar.js
+++ api/pagar.js
@@ -1,1 +1,51 @@
+export default async function handler(req,res){
 
+if(req.method!=="POST"){
+return res.status(405).end();
+}
+
+const {valor}=req.body;
+
+const resposta=
+await fetch(
+"https://api.mercadopago.com/checkout/preferences",
+{
+method:"POST",
+
+headers:{
+Authorization:
+`Bearer ${process.env.MP_TOKEN}`,
+
+"Content-Type":
+"application/json"
+},
+
+body:
+JSON.stringify({
+
+items:[
+
+{
+title:"Contribuição",
+quantity:1,
+currency_id:"BRL",
+unit_price:Number(valor)
+
+}
+
+]
+
+})
+
+}
+
+);
+
+const data=
+await resposta.json();
+
+res.json({
+url:data.init_point
+});
+
+}
