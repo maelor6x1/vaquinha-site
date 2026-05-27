4a53d1a94f86dce8924ef1bea0b6ef55d222cf53
maelor6x1 <r5y2vnn98p@privaterelay.appleid.com>
2026-05-27 15:19:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,72 @@
+const input =
+document.getElementById(
+"valor"
+);
+
+const meta = 5000;
+
+let arrecadado =
+Number(
+localStorage.getItem(
+"arrecadado"
+)
+)||0;
+
+atualizarBarra();
+
+function atualizarBarra(){
+
+const porcentagem =
+(arrecadado/meta)*100;
+
+document
+.getElementById(
+"bar"
+)
+.style.width =
+porcentagem+"%";
+
+document
+.getElementById(
+"valorMeta"
+)
+.innerText =
+
+`R$ ${arrecadado.toFixed(2)} arrecadados`;
+
+}
+
+input.addEventListener(
+"input",
+(e)=>{
+
+let v =
+e.target.value
+.replace(/\D/g,"");
+
+v =
+(v/100)
+.toLocaleString(
+"pt-BR",
+{
+minimumFractionDigits:2
+}
+);
+
+e.target.value =
+v==="0,00"
+?""
+:v;
+
+}
+);
+
 async function doar(){
 
 const valor =
-document
-.getElementById(
-"valor"
-)
-.value;
+input.value;
 
-const msg =
-document
-.getElementById(
-"msg"
-);
-
-if(
-!valor
-||
-Number(valor)<=0
-){
+if(!valor){
 
 alert(
 "Digite um valor"
@@ -27,8 +76,20 @@ return;
 
 }
 
-msg.innerHTML =
+const btn =
+document.getElementById(
+"btn"
+);
+
+btn.disabled=true;
+
+btn.innerText=
 "Gerando PIX...";
+
+const resultado =
+document.getElementById(
+"resultado"
+);
 
 try{
 
@@ -37,8 +98,7 @@ await fetch(
 "/api/pagar",
 {
 
-method:
-"POST",
+method:"POST",
 
 headers:{
 "Content-Type":
@@ -59,18 +119,16 @@ valor
 const data =
 await r.json();
 
-if(
-data.erro
-){
+if(data.erro){
 
-msg.innerHTML =
+resultado.innerHTML =
 
 `
 
 <pre>
 
 ${JSON.stringify(
-data,
+data.erro,
 null,
 2
 )}
@@ -79,56 +137,81 @@ null,
 
 `;
 
+btn.disabled=false;
+btn.innerText="Contribuir ❤️";
+
 return;
 
 }
 
-msg.innerHTML =
+resultado.innerHTML =
 
 `
 
 <h2>
 
-PIX Gerado
+Escaneie o PIX
 
 </h2>
 
 <img
-
 src="${data.qr}"
-
-style="
-width:220px;
-border-radius:16px;
-"
-
 >
 
-<br><br>
-
-<textarea
-
-style="
-width:100%;
-height:120px;
-"
-
-readonly
-
->
+<textarea readonly>
 
 ${data.pix}
 
 </textarea>
 
+<br><br>
+
+<button onclick="copiarPix()">
+Copiar código PIX
+</button>
+
 `;
+
+window.pixCode =
+data.pix;
+
+arrecadado +=
+parseFloat(
+valor
+.replace(".","")
+.replace(",",".")
+);
+
+localStorage.setItem(
+"arrecadado",
+arrecadado
+);
+
+atualizarBarra();
 
 }
 catch(e){
 
-msg.innerHTML =
+resultado.innerHTML =
 e.message;
 
 }
+
+btn.disabled=false;
+btn.innerText="Contribuir ❤️";
+
+}
+
+function copiarPix(){
+
+navigator
+.clipboard
+.writeText(
+window.pixCode
+);
+
+alert(
+"PIX copiado!"
+);
 
 }
